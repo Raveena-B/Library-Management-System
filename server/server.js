2ae22b22581f9ae45ffc275b086212d967e2b655
Raveena <raveenabandara801@gmail.com>
2024-06-28 11:49:15 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000 ;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser : true, useUnifiedTopology : true});
+
+const connection = mongoose.connection;
+connection.once('open' , () => {
+ console.log("MongoDB database connection established successfully");
+
+})
+
+// Routes
+const exampleRouter = require('./routes/example');
+app.use('/example', exampleRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
