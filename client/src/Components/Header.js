261935fb6e9b9bab9a366edcc62e57f00c148070
Raveena <raveenabandara801@gmail.com>
2024-07-03 12:30:49 +0530

    design the header component

---
+++ client/src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+    const Header = () => {
+        return (
+          <header className="header">
+            <div className="space" />            
+            <nav className="nav">
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/e-resources">E-Resources</a></li>
+                <li><a href="/personel">Personel</a></li>
+                <li><a href="/useOfLibrary">Use Of Library</a></li>
+                <li><a href="/aboutus">About Us</a></li>
+                <li><a href="/contactus">Contact Us</a></li>
+              </ul>
+            </nav>
+          </header>
+        )
+    }
+  
+
+
+export default Header;
